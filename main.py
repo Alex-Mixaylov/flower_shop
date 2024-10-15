c2b35cf271cb924709163e36cc8e098d40a69227
Alex <guardamar81@gmail.com>
2024-10-15 19:08:56 +0400

    create project
    create project structure

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+import logging
+import random
+import sqlite3
+import os
+from dotenv import load_dotenv
+
+import requests
