5d363417acd22822d75618c8c18230dde74a0b99
Alex <guardamar81@gmail.com>
2024-10-15 20:06:21 +0400

    urls
    urls

---
+++ flower_delivery_project/orders/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('shop', views.shop),
+    path('product-details', views.product_details),
+    path('thanks', views.thanks),
+    path('contact', views.contact),
+    path('collections', views.collections),
+    path('cart', views.cart),
+    path('checkout', views.checkout),
+]
--- flower_delivery_project/orders/views.py
+++ flower_delivery_project/orders/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Это Главная страница - index.html</h1>")
+
+def shop(request):
+    return HttpResponse("<h1>Это Страница магазина - shop.html</h1>")
+
+def product_details(request):
+    return HttpResponse("<h1>Это Страница конкретного товара - product-details.html</h1>")
+
+def thanks(request):
+    return HttpResponse("<h1>Это Страница благодарности за  заказ - thanks.html</h1>")
+
+def cart(request):
+    return HttpResponse("<h1>Это Страница корзина за  заказ - cart.html</h1>")
+
+def checkout(request):
+    return HttpResponse("<h1>Это Страница оформления за  заказ - checkout.html</h1>")
+
+def collections(request):
+    return HttpResponse("<h1>Это Страница Коллекций - collections.html</h1>")
+
+def contact(request):
+    return HttpResponse("<h1>Это Страница Контакты - contact.html</h1>")
