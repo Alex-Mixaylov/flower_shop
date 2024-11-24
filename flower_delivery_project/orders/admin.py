7faff5d933ed52ef12ca5e529f65fc64f26fc68d
Alex <guardamar81@gmail.com>
2024-11-25 00:08:44 +0400

    update admin.py
    update admin.py

--- flower_delivery_project/orders/admin.py
+++ flower_delivery_project/orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Collection, Bouquet, Order, OrderItem, ContactMessage
+from .models import User, Collection, Bouquet, Order, OrderItem, ContactMessage, BestSeller, TeamMember, Testimonial
 
 # Регистрация моделей в админке
 admin.site.register(User)
@@ -8,3 +8,19 @@ admin.site.register(Bouquet)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ContactMessage)
+
+@admin.register(BestSeller)
+class BestSellerAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'is_featured')
+    list_filter = ('is_featured',)
+    search_fields = ('title',)
+
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'role')
+    search_fields = ('name', 'role')
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('author',)
+    search_fields = ('author',)
