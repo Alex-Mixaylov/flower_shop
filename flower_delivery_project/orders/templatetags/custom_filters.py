5d936ccae227094fa5bcf515ce10cfcad6b9aaf6
Alex <guardamar81@gmail.com>
2024-12-09 20:18:34 +0400

    update
    update

--- flower_delivery_project/orders/templatetags/custom_filters.py
+++ flower_delivery_project/orders/templatetags/custom_filters.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 @register.filter
-def range(value):
+def range_filter(value):
     """Возвращает диапазон от 0 до value."""
     try:
         return range(int(value))
