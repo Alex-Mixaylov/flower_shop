c1e242c7246e7a4598c55bcad0b28ed41cd83f6d
Alex <guardamar81@gmail.com>
2025-01-07 22:50:41 +0400

    pagination
    pagination

--- flower_delivery_project/orders/templatetags/custom_filters.py
+++ flower_delivery_project/orders/templatetags/custom_filters.py
@@ -22,3 +22,19 @@ def float_subtract(value, arg):
         return float(value) - float(arg)
     except (ValueError, TypeError):
         return 0.0
+@register.filter
+def custom_pagination(paginator, current_page):
+    """
+    Возвращает список страниц для пагинации с учетом текущей страницы.
+    """
+    page_range = paginator.page_range
+    total_pages = paginator.num_pages
+    visible_pages = 5  # Количество отображаемых страниц
+
+    if total_pages <= visible_pages:
+        return page_range
+
+    start_page = max(current_page - 2, 1)
+    end_page = min(current_page + 2, total_pages)
+
+    return range(start_page, end_page + 1)
