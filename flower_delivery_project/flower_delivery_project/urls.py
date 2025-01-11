56e0266649db4b8325a3224231b6c31f75dabc7c
Alex <guardamar81@gmail.com>
2025-01-11 19:20:22 +0400

    update
    update

--- flower_delivery_project/flower_delivery_project/urls.py
+++ flower_delivery_project/flower_delivery_project/urls.py
@@ -3,9 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('orders.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='orders/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 # Добавляем обработку медиа-файлов в режиме разработки
