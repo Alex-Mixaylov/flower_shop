4b6192480c0bb97e99d0b6e931a1b0b9c57c9d32
Alex <guardamar81@gmail.com>
2025-01-11 20:57:12 +0400

    update login-logout
    update login-logout

--- flower_delivery_project/flower_delivery_project/urls.py
+++ flower_delivery_project/flower_delivery_project/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('orders.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='orders/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
 ]
 
 # Добавляем обработку медиа-файлов в режиме разработки
