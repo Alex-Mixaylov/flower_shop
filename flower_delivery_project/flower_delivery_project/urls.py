78994b57354449e7c026cbcdd73c984c6a460e0a
Alex <guardamar81@gmail.com>
2025-01-12 21:01:11 +0400

    update
    update

--- flower_delivery_project/flower_delivery_project/urls.py
+++ flower_delivery_project/flower_delivery_project/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib.auth import views as auth_views
+from ..orders import views as order_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
