b5f33b2fd9ba58b94f4105c1e9e50c63101586f8
Alex <guardamar81@gmail.com>
2025-01-16 23:56:20 +0400

    update OK
    update OK

--- flower_delivery_project/orders/forms.py
+++ flower_delivery_project/orders/forms.py
@@ -1,11 +1,24 @@
 from django import forms
+from django.forms import RadioSelect
 from .models import Review
+
+# Кастомный виджет
+class StarsRadioSelect(RadioSelect):
+    template_name = 'orders/widgets/radio_star.html'
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['rating', 'text']
         widgets = {
-            'rating': forms.RadioSelect(choices=[(i, f'{i} stars') for i in range(1, 6)]),
+            'rating': StarsRadioSelect(
+                choices=[
+                    (5, '5 stars'),
+                    (4, '4 stars'),
+                    (3, '3 stars'),
+                    (2, '2 stars'),
+                    (1, '1 star'),
+                ]
+            ),
             'text': forms.Textarea(attrs={'placeholder': 'Write your review here...'}),
         }
